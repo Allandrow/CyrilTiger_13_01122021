74a77da06365de0ae20cd8596f1c82c14f5ef403
Cyril Tiger <cyriltiger@gmail.com>
2022-01-05 16:39:28 +0100

    tests : added tokenSlice tests

--- src/__tests__/tokenSlice.test.ts
+++ src/__tests__/tokenSlice.test.ts
@@ -1,7 +1,22 @@
-import reducer from '../features/tokenSlice'
+import reducer, { removeToken, setToken } from '../features/tokenSlice'
 
 test('should return the initial state', () => {
   expect(reducer(undefined, { type: undefined })).toEqual({
+    token: null,
+  })
+})
+
+test('should update the token', () => {
+  const previousState = { token: null }
+  const token = 'New token'
+  expect(reducer(previousState, setToken(token))).toEqual({
+    token,
+  })
+})
+
+test('should remove the token', () => {
+  const previousState = { token: 'Old token' }
+  expect(reducer(previousState, removeToken())).toEqual({
     token: null,
   })
 })
