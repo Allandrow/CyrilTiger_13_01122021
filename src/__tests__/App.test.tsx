0593e16fbacb8b8aa46d38e04f5b6e4e603e819a
Cyril Tiger <cyriltiger@gmail.com>
2022-01-11 13:58:23 +0100

    added login page tests + mock implementation of useNavigate hook

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,14 @@
 import { render, screen } from '../../jest/test-utils'
 import { HomePage } from '../pages/HomePage'
+import { LoginPage } from '../pages/LoginPage'
+
+// Mocked useNavigate function
+const mockedUseNavigate = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUseNavigate,
+}))
 
 describe('Full app render/navigation', () => {
   test('Homepage renders correctly', () => {
@@ -7,5 +16,26 @@ describe('Full app render/navigation', () => {
     expect(
       screen.getByRole('heading', { name: 'Argent Bank' })
     ).toBeInTheDocument()
+  })
+  describe('Login Page', () => {
+    test('Login page renders correctly', () => {
+      render(<LoginPage />, { route: '/login', initialEntries: '/login' })
+      expect(
+        screen.getByRole('button', { name: /sign in/i })
+      ).toBeInTheDocument()
+    })
+    test('If already logged in, should redirect to homepage', () => {
+      const preloadedState = {
+        connection: {
+          status: 'connected',
+        },
+      }
+      render(<LoginPage />, {
+        preloadedState,
+        route: '/login',
+        initialEntries: '/login',
+      })
+      expect(mockedUseNavigate).toHaveBeenCalled()
+    })
   })
 })
