8ab02b6a4ebdcb3eef6bc6d14c4ce608afdf9fd6
Cyril Tiger <cyriltiger@gmail.com>
2022-01-13 16:37:10 +0100

    modified msw handlers + test login

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,20 +1,37 @@
 import 'whatwg-fetch'
 import { rest } from 'msw'
 
-const mockToken = 'Valid JWT'
+const mockToken = {
+  data: 'Valid JWT',
+}
 
 const mockUser = {
-  firstName: 'Tony',
-  lastName: 'Stark',
+  data: {
+    firstName: 'Tony',
+    lastName: 'Stark',
+  },
 }
 
 const baseUrl = 'http://localhost:3001/api/v1/user/'
 
+interface LoginReqBody {
+  email: string
+  password: string
+}
+
 export const handlers = [
-  rest.post(`${baseUrl}login`, (_, res, ctx) => {
+  rest.post(`${baseUrl}login`, (req, res, ctx) => {
+    console.log('REQUEST WITH', req.body)
+    const { email, password } = req.body as LoginReqBody
+    if (email.length === 0 || password.length === 0) {
+      console.log('400 BAD REQUEST')
+      return res(ctx.status(400), ctx.json({ message: 'Bad Request' }))
+    }
+    console.log('200 WITH', mockToken)
     return res(ctx.status(200), ctx.json(mockToken))
   }),
   rest.post(`${baseUrl}profile`, (_, res, ctx) => {
+    console.log('GET USER, SENDING BACK', mockUser)
     return res(ctx.status(200), ctx.json(mockUser))
   }),
   rest.put(`${baseUrl}profile`, (_, res, ctx) => {
--- src/pages/LoginPage.test.tsx
+++ src/pages/LoginPage.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, connectedPreloadedState } from '../../jest/test-utils'
 import { LoginPage } from './LoginPage'
+import userEvent from '@testing-library/user-event'
 
 const mockedUseNavigate = jest.fn()
 
@@ -25,3 +26,26 @@ test('If already logged in, should redirect to homepage', () => {
   })
   expect(mockedUseNavigate).toHaveBeenCalled()
 })
+
+test('submit filled form to redirect', async () => {
+  render(<LoginPage />, {
+    route: '/login',
+    initialEntries: '/login',
+  })
+  const userNameInput = screen.getByRole('textbox', { name: /username/i })
+  userEvent.type(userNameInput, 'tony@stark.com')
+  userEvent.type(screen.getByLabelText(/password/i), '123456')
+  userEvent.click(screen.getByRole('button', { name: /sign in/i }))
+  expect(mockedUseNavigate).toHaveBeenCalled()
+})
+
+// test('submit error should throw', async () => {
+//   render(<LoginPage />, {
+//     route: '/login',
+//     initialEntries: '/login',
+//   })
+//   const userNameInput = screen.getByRole('textbox', { name: /username/i })
+//   userEvent.type(userNameInput, 'tony@stark.com')
+//   userEvent.click(screen.getByRole('button', { name: /sign in/i }))
+//   expect(mockedUseNavigate).not.toHaveBeenCalled()
+// })
